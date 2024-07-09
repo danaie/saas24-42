007c4654473d44ca08a8fb43d16f80d2f7a3095b
ntua-el20812 <147840584+ntua-el20812@users.noreply.github.com>
2024-07-09 12:53:14 +0300

    Fixed bugs -Finished Sub

--- finished_sub/connect_db.js
+++ finished_sub/connect_db.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.FINISHED_DB, process.env.FINISHED_DB_USER, process.env.FINISHED_DB_PASS, {
+const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASS, {
     dialect: 'mysql',
-    host: process.env.FINISHED_DB_HOST,
-    port: process.env.FINISHED_DB_PORT
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
 });
 
 sequelize.authenticate()
@@ -12,7 +12,11 @@ sequelize.authenticate()
     console.log("Success connecting to finished database!"); 
 })
 .catch(err => {
-    console.error(`Unable to connect to the finished database user ${process.env.FINISHED_DB_USER}`, err);
+    console.error(`Unable to connect to the finished database user ${process.env.DB_USER}`, err);
 })
 
 module.exports = sequelize;
+
+
+
+
