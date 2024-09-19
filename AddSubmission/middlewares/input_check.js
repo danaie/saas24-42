f1c883a42911f26237f7cc2fe9c3a5abb854688b
ntua-el20850 <el20850@mail.ntua.gr>
2024-09-20 01:43:32 +0300

    Imrovements on Add Submission and added notes on Credit Transaction READ.me

--- AddSubmission/middlewares/input_check.js
+++ AddSubmission/middlewares/input_check.js
@@ -1,33 +1,51 @@
-const inputCheck = (req, res, next) => {
+const axios = require('axios').default;
 
-    const keys = Object.keys(req.body);
-    if (keys.length !== 8 || keys[0] !== 'locations' || keys[1] !== 'num_vehicles' || keys[2] !== 'depot' || keys[3] !== 'max_distance' || keys[4] !== 'user_id' || keys[5] !== 'username' || keys[6] !== 'submission_name' || keys[7] !== 'timestamp') {
-        return res.status(400).send('Wrong parameters in Request Body');
-    }
-
-    const { locations, num_vehicles, depot, max_distance, user_id, username, submission_name, timestamp} = req.body;
-
-    for (let loc of locations) {
-        const locKeys = Object.keys(loc);
-
-        if (locKeys.length !== 2 || locKeys[0] !== 'Latitude' || locKeys[1] !== 'Longitude') {
-            return res.status(400).send('Wrong parameters in locations');
+const inputCheck = async (req, res, next) => {
+    try
+    {
+        const keys = Object.keys(req.body);
+        if (keys.length !== 8 || keys[0] !== 'locations' || keys[1] !== 'num_vehicles' || keys[2] !== 'depot' || keys[3] !== 'max_distance' || keys[4] !== 'user_id' || keys[5] !== 'username' || keys[6] !== 'submission_name' || keys[7] !== 'timestamp') {
+            return res.status(400).send('Wrong parameters in Request Body');
         }
 
-        if (typeof loc.Latitude !== 'number' || typeof loc.Longitude !== 'number') {
-            return res.status(400).send('Latitude and Longitude must be numbers');
+        const { locations, num_vehicles, depot, max_distance, user_id, username, submission_name, timestamp} = req.body;
+
+        for (let loc of locations) {
+            const locKeys = Object.keys(loc);
+
+            if (locKeys.length !== 2 || locKeys[0] !== 'Latitude' || locKeys[1] !== 'Longitude') {
+                return res.status(400).send('Wrong parameters in locations');
+            }
+
+            if (typeof loc.Latitude !== 'number' || typeof loc.Longitude !== 'number') {
+                return res.status(400).send('Latitude and Longitude must be numbers');
+            }
         }
-    }
 
-    if (typeof num_vehicles !== 'number' || typeof depot !== 'number' || typeof max_distance !== 'number' || typeof user_id !== 'number') {
-        return res.status(400).send('num_vehicles, depot, max_distance and user_id must be numbers');
-    }
-    console.log(timestamp)
-    if (typeof username !== 'string' || typeof submission_name !== 'string' || typeof timestamp !== 'string') {
-        return res.status(400).send('username, submission_name, timestamp should be strings');
-    }    
+        if (typeof num_vehicles !== 'number' || typeof depot !== 'number' || typeof max_distance !== 'number' || typeof user_id !== 'number') {
+            return res.status(400).send('num_vehicles, depot, max_distance and user_id must be numbers');
+        }
+        console.log(timestamp)
+        if (typeof username !== 'string' || typeof submission_name !== 'string' || typeof timestamp !== 'string') {
+            return res.status(400).send('username, submission_name, timestamp should be strings');
+        }
+        
+        const data = {
+            "amount": -50,
+            "user_id": user_id
+        }
 
-    next();
+        const response = await axios.post(`http://localhost:1000/editCredits`, data);
+        if(response.status === 200){
+            next();
+        }else{
+            res.status(406).send('Not enough credits')
+        }
+        
+    }catch (error) {
+        console.error('Error while processing the request:', error.message || error);
+        res.status(500).send('Internal Server Error.');
+    }
 };
 
 module.exports = inputCheck;
--- credit_transaction/README.md
+++ credit_transaction/README.md
@@ -3,3 +3,18 @@
 ## (name)
 
 (description)
+
+PORT: 1000
+
+POST /editCredits:
+
+body 
+{
+	"amount": +/- X
+	"user_id": Y
+}
+
+
+Response Codes: 
+200: Transaction Done
+406: Not enough credits
