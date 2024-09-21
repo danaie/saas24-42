044aa2dfbb5295fc14708d94fb0d9b081e3bb855
ntua-el20111 <el20111@mail.ntua.gr>
2024-09-21 18:09:52 +0300

    Frontend

---
+++ frontend/app/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white text-center p-5">
+        <p>&copy; 2024 Made by saas 42</p>
+      </footer>
+    )
+}
+
+export default Footer;
---
+++ frontend/app/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="bg-gray-800 text-white p-5 text-center">
+        <h1 className="text-4xl font-bold">solveMe</h1>
+      </header>
+    )
+}
+
+export default Header;
