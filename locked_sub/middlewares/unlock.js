a00f85fa430ad2f44f563a7b09f4f68ffa454ece
ntua-el20111 <149299165+ntua-el20111@users.noreply.github.com>
2024-09-30 02:55:54 +0300

    Back to correct unlock.js

--- locked_sub/middlewares/unlock.js
+++ locked_sub/middlewares/unlock.js
@@ -13,12 +13,10 @@ const unlock = async(req, res, next) => {
         const cost = costArray[0].extra_credits
         const user_id = costArray[0].user_id
         console.log('Extra credits needed', cost)
-
         const data = {
             "amount": -1*cost,
             "user_id": user_id
         }
-
         console.log(data)
         const response = await axios.post(`http://credit-transaction:8080/edit_credits`, data);
         if(response.status === 200){
@@ -39,16 +37,13 @@ const unlock = async(req, res, next) => {
                   // });
               
                   // channel.sendToQueue(queue, Buffer.from(msg));
-
                   //console.log(" [x] Sent %s", msg);
                   const exchange = 'finished_submission';
                   const msg = JSON.stringify(del);
                   console.log('Message to be sent:', msg);
-
                   // Assert the exchange and publish the message
                   channel.assertExchange(exchange, 'fanout', { durable: false });
                   channel.publish(exchange, '', Buffer.from(msg)); // Fanout ignores routing key
-
                   console.log(" [x] Sent message to exchange %s: %s", exchange, msg);
                 });
               });
@@ -57,10 +52,16 @@ const unlock = async(req, res, next) => {
              res.status(406).send('Not enough credits')
         }
     }catch(error){
-        console.error('Error while processing the request:', error.message || error);
-        res.status(500).send('Internal Server Error.');
+        if (error.response.status === 406) {
+            return res.status(406).json({ message: "Not enough credits." });
+        }else {
+            console.error('Error while processing the request:', error.message || error);
+            res.status(500).send('Internal Server Error.');
+        }
     }
+    
 }
+
 
 
 
